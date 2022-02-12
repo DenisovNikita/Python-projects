c60dd9118c2837e8489cc76830498d36b48aaf4b
Nikita Denisov <nikita160412@yandex.ru>
2022-02-12 20:09:17 +0700

    Rewrite according to functional style

--- hw2/src/gen_latex_table.py
+++ hw2/src/gen_latex_table.py
@@ -1,49 +1,66 @@
-def add_packages(s):
-    return s + '\\usepackage[utf8]{inputenc}\n'
-
-
-def add_title(s):
-    return s + '\\title{Test}\n'
-
-
-def add_author(s):
-    return s + '\\author{Niktita Denisov}\n'
-
-
 def init():
-    s = '\\documentclass{article}\n'
-    return add_author(add_title(add_packages(s)))
+    return LatexMaker('\\documentclass{article}\n'). \
+        add_packages(). \
+        add_title(). \
+        add_author()
 
 
-def write_table(table):
-    n = len(table)
-    s = ''
-    if n == 0:
-        return s
-    m = len(table[0])
-    colons = '|' + 'c|' * m
-    s = '\\begin{tabular}{' + colons + '}\n'
-    s = s + '\\hline\n'
-    for i in range(n):
-        row = ''
-        for j in range(m):
-            row = row + table[i][j]
-            if j + 1 < m:
-                row = row + ' & '
-            else:
-                row = row + '\\\\'
-        s = s + row + '\n'
-        s = s + '\\hline\n'
-    s = s + '\\end{tabular}\n'
-    return s
+def make_latex(table):
+    return init().write_document(table)
 
 
-def write_document(s, table):
-    t = s + '\\begin{document}\n'
-    t = t + '\\maketitle\n'
-    t = t + write_table(table)
-    t = t + '\\end{document}\n'
-    return t
+class LatexMaker:
+
+    def __init__(self, s):
+        self.s = s
+
+    def __str__(self):
+        return self.s
+
+    def add_packages(self):
+        return LatexMaker(self.s + '\\usepackage[utf8]{inputenc}\n')
+
+    def add_title(self):
+        return LatexMaker(self.s + '\\title{Test}\n')
+
+    def add_author(self):
+        return LatexMaker(self.s + '\\author{Niktita Denisov}\n')
+
+    def write_table(self, table):
+        n = len(table)
+        s = ''
+        if n > 0:
+            m = len(table[0])
+            colons = '|' + 'c|' * m
+            s = '\\begin{tabular}{' + colons + '}\n'
+            s += '\\hline\n'
+            for i in range(n):
+                row = ''
+                for j in range(m):
+                    row = row + table[i][j]
+                    if j + 1 < m:
+                        row += ' & '
+                    else:
+                        row += '\\\\'
+                s += row + '\n'
+                s += '\\hline\n'
+            s += '\\end{tabular}\n'
+        return LatexMaker(self.s + s)
+
+    def add_begin(self, name):
+        return LatexMaker(self.s + '\\begin{' + name + '}\n')
+
+    def add_end(self, name):
+        return LatexMaker(self.s + '\\end{' + name + '}\n')
+
+    def make_title(self):
+        return LatexMaker(self.s + '\\maketitle\n')
+
+    def write_document(self, table):
+        return self.add_begin('document'). \
+            make_title(). \
+            write_table(table). \
+            add_end('document')
 
 
 input_table = [
@@ -52,8 +69,7 @@ input_table = [
     ['7', '8', '9'],
 ]
 
-latex = init()
-latex = write_document(latex, input_table)
+latex = make_latex(input_table)
 
 with open('../artefacts/easy/table.tex', 'w') as f:
-    f.write(latex)
+    f.write(str(latex))
